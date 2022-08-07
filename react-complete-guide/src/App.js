66a1e52f073f59d5c47ae8b6a58c40496a08aa97
Tom Victor <vjtomvictor@icloud.com>
2022-08-07 23:54:41 +0530

    add props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,10 +1,48 @@
-import ExpenceItem from './components/ExpenceItem'
+import ExpenceItem from "./components/ExpenceItem";
 
 function App() {
+  const data = [
+    {
+      id : 1,
+      date : new Date(2022, 7, 8).toISOString(),
+      title: "Car Insurance",
+      description: "Volkswagen car",
+      price: 90,
+    },
+    {
+      id : 2,
+      date : new Date(2022, 7, 8).toISOString(),
+      title: "Paper",
+      description: "Just paper",
+      price: 4242,
+    },
+    {
+      id : 3,
+      date : new Date(2022, 7, 8).toISOString(),
+      title: "Laptop",
+      description: "Apple",
+      price: 23424,
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenceItem></ExpenceItem>
+      <ExpenceItem
+        date={data[0].date}
+        description={data[0].description}
+        price={data[0].price}
+      ></ExpenceItem>
+      <ExpenceItem
+        date={data[1].date}
+        description={data[1].description}
+        price={data[1].price}
+      ></ExpenceItem>
+      <ExpenceItem
+        date={data[2].date}
+        description={data[2].description}
+        price={data[2].price}
+      ></ExpenceItem>
+
     </div>
   );
 }
--- react-complete-guide/src/components/ExpenceItem.js
+++ react-complete-guide/src/components/ExpenceItem.js
@@ -1,18 +1,15 @@
-import './ExpenceItem.css' ;
+import "./ExpenceItem.css";
 
-function ExpenceItem() {
-    const expenseDate = new Date(2022, 7, 8);
-    const expenseDesription = 'Car Insurance';
-    const expensePrice = 90 ;
-    return (
-        <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
-            <div className='expense-item__description'>
-                <h1>{expenseDesription}</h1>
-                <div className='expense-item__price'>${expensePrice}</div>
-            </div>
-        </div>
-    );
+function ExpenceItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date}</div>
+      <div className="expense-item__description">
+        <h1>{props.description}</h1>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenceItem;
