c989779bc639dac0fff67db51b8e0afdac89b96f
Tom Victor <vjtomvictor@icloud.com>
2022-08-08 17:18:21 +0530

    Card composition

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h1>{props.description}</h1>
         <div className="expense-item__price">${props.price}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- react-complete-guide/src/components/Expenses.js
+++ react-complete-guide/src/components/Expenses.js
@@ -1,9 +1,10 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         date={props.data[0].date}
         description={props.data[0].description}
@@ -19,7 +20,7 @@ function Expenses(props) {
         description={props.data[2].description}
         price={props.data[2].price}
       />
-    </div>
+    </Card>
   );
 }
 
