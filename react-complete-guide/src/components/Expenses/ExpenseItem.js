66f71f2eb7ba89a8e885b5dd13d38e004692991c
Tom Victor <vjtomvictor@icloud.com>
2022-08-08 19:59:09 +0530

    button event handle

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -3,12 +3,16 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const buttonHandler = () => {
+    console.log("Clicked!!!") ;
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h1>{props.description}</h1>
         <div className="expense-item__price">${props.price}</div>
+        <button onClick={buttonHandler}>Click me</button>
       </div>
     </Card>
   );
