babdffb4a99397557c73314949cf9ea40326d2b9
Tom Victor <vjtomvictor@icloud.com>
2022-08-13 00:40:34 +0530

    add chart

---
+++ react-complete-guide/src/components/Chart/Chart.js
@@ -0,0 +1,12 @@
+import 'Chart.css'
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+    return (
+        <div>
+            <ChartBar/>
+        </div>
+    )
+};
+
+export default Chart;
---
+++ react-complete-guide/src/components/Chart/ChartBar.js
@@ -0,0 +1,7 @@
+import 'ChartBar.css'
+
+const ChartBar = (props) => {
+    return ""
+};
+
+export default ChartBar;
