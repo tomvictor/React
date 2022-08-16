d877cd6f46120e2ebdb6ea84e3b1b792dac7c43d
Tom Victor <vjtomvictor@icloud.com>
2022-08-17 00:09:49 +0530

    fix bar chart (working)

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filterDate, setFilterDate] = useState("2022");
@@ -22,6 +23,7 @@ const Expenses = (props) => {
                 selectedValue={filterDate}
                 onFilterDate={filterChangeHandler}
             />
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses} />
         </Card>
     </div>
--- react-complete-guide/src/components/Expenses/ExpensesChart.js
+++ react-complete-guide/src/components/Expenses/ExpensesChart.js
@@ -16,7 +16,12 @@ const ExpensesChart = (props) => {
         { label : "Nov", value : 0},
         { label : "Dec", value : 0},
     ];
-    return <Chart/>
+
+    for (const expense of props.expenses){
+        const expenseMonth = expense.date.getMonth();
+        chartDataPoints[expenseMonth].value += expense.amount;
+    }
+    return <Chart dataPoints={chartDataPoints}/>
 }
 
 export default ExpensesChart;
